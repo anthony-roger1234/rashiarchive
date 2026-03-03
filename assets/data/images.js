5776cf9dc4273f71862ccd8e187d8880e4cdd1b4
anthony-roger1234 <111300442+anthony-roger1234@users.noreply.github.com>
2026-03-03 15:04:14 +0800

    Update images.js

--- assets/data/images.js
+++ assets/data/images.js
@@ -1,5 +1,5 @@
 const imageData = [
-    { file: "Amers Martha.jpg", link: "https://google.com" },
+    { file: "Amers Martha.jpeg", link: "https://google.com" },
     { file: "Ayesha Oprenario.jpg", link: "https://example.com/2" },
-    { file: "Kauri Lorraine.png", link: "https://example.com/3" }
+    { file: "Kauri Lorraine.jpg", link: "https://example.com/3" }
 ];

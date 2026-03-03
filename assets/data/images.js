b83918043bb70ed78e25f2c33c8b0c4658272a63
anthony-roger1234 <111300442+anthony-roger1234@users.noreply.github.com>
2026-03-03 14:46:58 +0800

    Create images.js

---
+++ assets/data/images.js
@@ -0,0 +1,5 @@
+const imageData = [
+    { file: "Amers Martha.jpg", link: "https://example.com/1" },
+    { file: "Ayesha Oprenario.jpg", link: "https://example.com/2" },
+    { file: "Kauri Lorraine.png", link: "https://example.com/3" }
+];

654b6e741f8534c82e34ff8010a1573958884724
anthony-roger1234 <111300442+anthony-roger1234@users.noreply.github.com>
2026-03-03 14:37:50 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,1 @@
-// Mobile menu toggle
-const menuToggle = document.getElementById("menuToggle");
-const navLinks = document.querySelector(".nav-links");
-
-menuToggle.addEventListener("click", () => {
-    if (navLinks.style.display === "flex") {
-        navLinks.style.display = "none";
-    } else {
-        navLinks.style.display = "flex";
-    }
-});
-
-// Button click interaction
-const button = document.querySelector(".btn");
-
-button.addEventListener("click", () => {
-    alert("Future customization goes here!");
-});
+// Future custom scripts will go here
